bc129f474c43f528606f40d9b07334c61058f725
Germanized <91925088+Germanized@users.noreply.github.com>
2023-09-25 16:59:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,45 @@ document.addEventListener("DOMContentLoaded", function() {
     const videoURL = document.getElementById("videoURL");
     const resultDiv = document.getElementById("result");
     const progressDiv = document.getElementById("progress");
-
-    const convertButton = document.querySelector("button");
     const downloadLink = document.getElementById("downloadLink");
 
-    convertButton.addEventListener("click", function() {
+    function updateProgress() {
+        let progress = 0;
+        const dots = ["", ".", "..", "..."];
+        const progressInterval = setInterval(function() {
+            progressDiv.textContent = "Converting" + dots[progress % 4];
+            progress++;
+        }, 1000);
+
+        return progressInterval;
+    }
+
+    function completeConversion() {
+        clearInterval(progressInterval);
+        progressDiv.style.display = "none";
+        resultDiv.innerHTML = "Conversion complete!";
+        downloadLink.href = "#"; // Set your download link here
+        downloadLink.style.display = "block";
+    }
+
+    let progressInterval;
+
+    function convertVideo() {
         const videoURLValue = videoURL.value;
 
         if (videoURLValue) {
             // Show the progress bar
             progressDiv.style.display = "block";
-            progressDiv.innerHTML = "Converting...";
+            progressInterval = updateProgress();
 
             // Simulate a conversion delay (replace with actual logic)
-            let progress = 0;
-            const progressInterval = setInterval(function() {
-                progress++;
-                progressDiv.innerHTML = "Converting" + ".".repeat(progress % 4);
-                if (progress === 3) {
-                    clearInterval(progressInterval);
-
-                    // Hide the progress bar when the conversion is complete
-                    progressDiv.style.display = "none";
-
-                    resultDiv.innerHTML = "Conversion complete!";
-                    // Display the download link
-                    downloadLink.href = "#"; // Set your download link here
-                    downloadLink.textContent = "Download MP3";
-                    downloadLink.style.display = "block";
-                }
-            }, 1000);
+            setTimeout(completeConversion, 3000);
         } else {
             resultDiv.innerHTML = "Please enter a valid YouTube URL.";
         }
-    });
+    }
+
+    // Attach the `convertVideo` function to the button's click event
+    const convertButton = document.querySelector("button");
+    convertButton.addEventListener("click", convertVideo);
 });
